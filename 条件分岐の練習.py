b739cdfc0bcf4a13a6bde833ac3234afb3686710
花音 <kihiro.0422.max@gmail.com>
2024-11-18 11:38:48 +0900

    Update 条件分岐の練習.py
    修正版
    tryをまとめた。

--- 条件分岐の練習.py
+++ 条件分岐の練習.py
@@ -1,12 +1,7 @@
 
 x=input("数字を入力")
-error=0
 try:
     x=int(x)
-except:
-    print("エラー（数字ではない可能性があります）")
-    error=1
-try:
     if x==10:
         print("１０です")
     elif x>10:
@@ -14,6 +9,5 @@ try:
     else:
         print("１０より小さいです")
 except:
-    if error==0:
-        print("エラー（数字ではない可能性があります）")
-print("最終結果：x=={},type(x)=={},error=={}".format(x,type(x),error))
+    print("エラー（数字ではない可能性があります）")
+print("最終結果：x=={},type(x)=={},error=={}".format(x,type(x)))
