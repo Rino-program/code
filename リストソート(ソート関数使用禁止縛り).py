bdc746adebd0dd9337190d97c880c5c865bc4846
花音 <kihiro.0422.max@gmail.com>
2024-11-22 23:36:45 +0900

    Update リストソート(ソート関数使用禁止縛り).py

--- リストソート(ソート関数使用禁止縛り).py
+++ リストソート(ソート関数使用禁止縛り).py
@@ -1,9 +1,9 @@
 def li(x):
-  if len(x)<2:
-    return x
-  for i in range(len(x)-1):
-    if x[i] < x[i+1] or x[i] == x[i+1]:
-      pass
-    elif x[i] > x[i+1]:
-      x.insert(i,x[i+1])
+    if len(x)<2:
+        return x
+    for i in range(len(x)-1):
+        if x[i] < x[i+1] or x[i] == x[i+1]:
+            pass
+        elif x[i] > x[i+1]:
+            x.insert(i,x[i+1])
 print(li(list(map(int,input("リストを入力してください。").split(",")))))
