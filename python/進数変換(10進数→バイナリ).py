1ff0e0ed3aa9eb8fe48f666455fc8b40f47c8c48
花音 <kihiro.0422.max@gmail.com>
2024-11-24 17:55:19 +0900

    test-a

--- python/進数変換(10進数→バイナリ).py
+++ python/進数変換(10進数→バイナリ).py
@@ -1,11 +1,10 @@
 def ten_two(x):
     x=int(x)
     y=[]
-    xa=x
     while 0<x:
         y.append(x%2)
         x=x//2
     if y==[]:
         y.append(0)
-    return int("".join(map(str, y[::-1])))
+    return int("".join(list(map(str, y[::-1]))))
 print(ten_two(input("10進数→2進数変換器\n10進数を入力してください。")))
