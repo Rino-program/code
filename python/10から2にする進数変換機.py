1720f62f21c965e992299a24822b37e3d7268ec3
花音 <kihiro.0422.max@gmail.com>
2024-11-26 04:49:56 +0000

    test 改良

--- python/10から2にする進数変換機.py
+++ python/10から2にする進数変換機.py
@@ -1,7 +1,13 @@
 def ten_two(ten):
-    li=[]
+    x,li=ten,[]
+    ten=abs(ten)
     while ten!=0:
         li.insert(0,ten%2)
         ten = ten//2
-    return "".join(map(str,li))
-print(int(ten_two(int(input("10進数を入力してください。(自然数のみ対応)")))))
+    if x<0:
+        li.insert(0,1)
+    else:
+        li.insert(0,0)
+    li="".join(map(str,li))
+    return li
+print(ten_two(int(input("10進数を入力してください。(整数のみ対応)"))))
