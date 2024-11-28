62a2fa1895677aa19c6cc25ed77f656fe850228b
花音 <kihiro.0422.max@gmail.com>
2024-11-28 15:00:04 +0000

    up-c-2-1

--- python/問題/#Microsoft Copilotからの出題(練習) ～素数判定～.py
+++ python/問題/#Microsoft Copilotからの出題(練習) ～素数判定～.py
@@ -2,7 +2,7 @@
 def pnf(x):
     if x==1 or x%2==0:
         return False
-    for i in range(3,int(x**(1/2)),2):
+    for i in range(3,int(x**(1/2))+1,2):
         if x%i==0:
             return False
     return True
