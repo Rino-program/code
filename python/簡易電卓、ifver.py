c0d30749e06e1423af745883981d17e43385ecd4
花音 <kihiro.0422.max@gmail.com>
2024-11-29 14:13:20 +0000

    up-f

---
+++ python/簡易電卓、ifver.py
@@ -0,0 +1,19 @@
+#Microsoft Copilotからの出題(練習) ～簡易電卓 if ver～
+def calculator(x):
+    li=x.split()
+    a,b,c=int(li[0]),li[1],int(li[2])
+    if b=="+":
+        return a+c
+    elif b=="-":
+        return a-c
+    elif b=="*":
+        return a*c
+    elif b=="/":
+        return a/c
+    elif b=="**":
+        return a**c
+    elif b=="//":
+        return a//c
+    else:
+        return "エラーです。"
+print(calculator(input("計算式を入力してください(例:5 + 3)")))
