b7190320f578703b77cddaeacc5a3131c485e4f4
花音 <kihiro.0422.max@gmail.com>
2024-12-02 14:48:16 +0000

    up-g-4

--- python/完全数チェッカー.py
+++ python/完全数チェッカー.py
@@ -3,5 +3,7 @@ def pn(num):
     for i in range(1,(num**(1/2))+1):
         if num%i==0:
             li.append(i)
+        if i!=num//i:
+            li.append(num//i)
     return sum(li)==num
 print(pn(int(input("数を入力"))))
