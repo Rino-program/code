10e91aa7760342f2eb2c5234ca3d6cd60c024f1c
花音 <kihiro.0422.max@gmail.com>
2024-12-03 14:50:11 +0900

    up-h-2

--- python/問題/# Microsoft Copilotからの出題(練習) ～ピタゴラスの三つ組の列挙～.py
+++ python/問題/# Microsoft Copilotからの出題(練習) ～ピタゴラスの三つ組の列挙～.py
@@ -1,10 +1,10 @@
 # Microsoft Copilotからの出題(練習) ～ピタゴラスの三つ組の列挙～
 def fpt(num):
-    lit=[]
+    li=[]
     for i in range(1,num):
         for j in range(i,num):
             for k in range(j,num):
                 if i**2+j**2==k**2:
-                    lit.append((i,j,k))
-    return lit
+                    li.append((i,j,k))
+    return li
 print(fpt(int(input("数を入力"))))
