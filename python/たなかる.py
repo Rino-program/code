0cc1ff85aefcacc33ea75a1114d1b5eab99a2a17
花音 <kihiro.0422.max@gmail.com>
2024-12-10 14:30:31 +0000

    up

--- python/たなかる.py
+++ python/たなかる.py
@@ -1,9 +1,9 @@
 def tnkl(num):
     lit=[1,1]
     for i in range(num):
+        print(lit)
         li=[1,1]
         for j in range(len(lit)-1):
             li.insert(j+1,lit[j]+lit[j+1])
         lit=li
-        print(lit)
 tnkl(int(input("段数")))
