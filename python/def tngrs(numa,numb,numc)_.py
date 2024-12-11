d9db93703d5f750a7c179bfe20654f92b4657b5d
花音 <kihiro.0422.max@gmail.com>
2024-12-12 02:07:36 +0900

    Add files via upload

---
+++ python/def tngrs(numa,numb,numc)_.py
@@ -0,0 +1,37 @@
+def tngrs(numa,numb,numc):
+    x_index,cr=0,0
+    if not numa:
+        x_index+=3
+    if not numb:
+        x_index+=4
+    if not numc:
+        x_index+=5
+    if x_index>6 or x_index<3:
+        return "エラーです。(パターン1)"
+    match x_index:
+        case 3:
+            cr=(numc**2-numb**2)
+        case 4:
+            cr=(numc**2-numa**2)
+        case 5:
+            cr=(numa**2+numb**2)
+        case _:
+            return "エラーです。(パターン2)"
+    if cr**(1/2)==int(cr**(1/2)):
+        return cr**(1/2)
+    else:
+        return "√"+str(cr)
+an,bn,cn=0,0,0
+try:
+    an=int(input("aを入力"))
+except:
+    pass
+try:
+    bn=int(input("bを入力"))
+except:
+    pass
+try:
+    cn=int(input("cを入力"))
+except:
+    pass
+print(tngrs(an,bn,cn))
