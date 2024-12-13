45c4546c9f79ce71697350dfe6a3326334be6d5a
花音 <kihiro.0422.max@gmail.com>
2024-12-13 01:39:30 +0000

    upup

--- python/.py
+++ python/.py
@@ -1,1 +1,20 @@
-
+def heihoukon(num):
+    import math
+    lia,hei,num=[],0,str(num)
+    if "√" in num:
+        if num[0].isdigit():
+            lia=num.split("√")
+            num=math.sqrt((lia[0]**2)*lia[1])
+            hei=1
+        elif num[0]=="√":
+            li=[]
+            def pnf(x):
+                if x%2==0:
+                    return False
+                for i in range(3,int(x**(1/2))+1,2):
+                    if x%i==0:
+                        return False
+                return True
+            for i in range(2,int(num[1:])/2):
+                if pnf(i):
+                    
