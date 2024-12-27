79a2f4b78459d7880a28eb59c3c924b8accb9404
花音 <kihiro.0422.max@gmail.com>
2024-12-28 00:42:21 +0900

    Update 友愛数検索.py

--- python/友愛数検索.py
+++ python/友愛数検索.py
@@ -1,9 +1,8 @@
 def pn(num):
     lit=[]
-    for j in range(220,num+1):
-        numr=j
-        numr_keep,t=numr,0
-        for _ in range(2):
+    for numr in range(220,num+1):
+        numr_keep=numr
+        for j in range(2):
             li=[1]
             for i in range(2,int(numr**(1/2))+1):
                 if numr%i==0:
@@ -11,9 +10,8 @@ def pn(num):
                     if i!=numr//i:
                         li.append(numr//i)
             numr=sum(li)
-            if t==0:
+            if j==0:
                 num_keep2=sum(li)
-                t=1
         if numr_keep==numr and numr!=num_keep2 and numr<num_keep2:
             lit.append((numr,num_keep2))
     return lit
