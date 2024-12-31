19e196df7dca25d7da76efcb21df752b4ea67072
花音 <kihiro.0422.max@gmail.com>
2024-12-31 10:47:50 +0900

    up-l-2

--- python/bunsuu.py
+++ python/bunsuu.py
@@ -6,6 +6,6 @@ numerator = int(x*(10**y))
 denominator = 10**y
 for i in range(numerator,1,-1):
     if numerator % i == 0 and denominator % i == 0:
-        numerator = numerator //i
+        numerator = numerator // i
         denominator = denominator // i
 print(f"{numerator}/{denominator}")
